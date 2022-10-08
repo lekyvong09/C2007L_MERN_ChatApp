4f3a447ea375fa790587c2b5b26d2e0361ac4b6c
lekyvong09 <lekyvong09@gmail.com>
2022-10-08 20:16:50 +0700

    react: dashboard login register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LoginPage from './features/auth/LoginPage';
+import RegisterPage from './features/auth/RegisterPage';
+import DashboardPage from './features/dashboard/DashboardPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route index element={<DashboardPage />} />
+      <Route path='login' element={<LoginPage />} />
+      <Route path='register' element={<RegisterPage />} />
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
   );
 }
 
