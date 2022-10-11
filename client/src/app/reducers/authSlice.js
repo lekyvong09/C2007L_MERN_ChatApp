b54c55ef5373f1d2056e85805b533abcdebc00d7
lekyvong09 <lekyvong09@gmail.com>
2022-10-11 21:04:34 +0700

    handle error in log-in

--- client/src/app/reducers/authSlice.js
+++ client/src/app/reducers/authSlice.js
@@ -3,13 +3,26 @@ import * as api from '../../api';
 
 export const loginThunk = createAsyncThunk(
     'auth/login',
-    async (userLoginData) => {
+    async (userLoginData, thunkAPI) => {
         try {
             const response = await api.login(userLoginData);
-            console.log(response);
+
+            if (response.error && response.exception.response.data) {
+                console.log(response.exception.response.data.message);
+            } else if (response.error && response.exception) {
+                console.log(response.exception.message);
+            } else if (response.error) {
+                console.log(response);
+            }
+            
+            localStorage.setItem('user', JSON.stringify(response.data));
             return response.data;
         } catch (error) {
             console.log(error);
+            if (!error.response) {
+                throw error;
+            }
+            return thunkAPI.rejectWithValue(error.response.data);
         }
     }
 );
@@ -26,7 +39,15 @@ export const authSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(loginThunk.fulfilled, (state, action) => {
             state.userDetails = action.payload;
-        })
+        });
+
+        builder.addCase(loginThunk.rejected, (state, action) => {
+            if (action.payload) {
+                console.log(action.payload);
+            } else {
+                console.log(action.error);
+            }
+        });
     },
 });
 
