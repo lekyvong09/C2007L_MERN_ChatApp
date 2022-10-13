135f265711274eb440dfdc0f1cf77e751ffd7b8a
lekyvong09 <lekyvong09@gmail.com>
2022-10-13 21:11:19 +0700

    dashboard layout

---
+++ client/src/features/dashboard/AddFriendDialog.js
@@ -0,0 +1,49 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import InputWithLabel from "../../app/layout/InputWithLabels";
+import PrimaryButton from "../../app/layout/PrimaryButton";
+import { validateEmail } from "../../app/validators/validator";
+
+
+export default function AddFriendDialog({open, closeAddFriendDialog}) {
+    const [mail, setMail] = useState('');
+    const [isFormValid, setIsFormValid] = useState('');
+
+    useEffect(() => {
+        setIsFormValid(validateEmail(mail));
+    }, [mail]);
+
+    const handleSendInvitation = () => {
+
+    }
+
+    return (
+        <Dialog open={open} onClose={closeAddFriendDialog}>
+            <DialogTitle>Invite a Friend</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Enter email address of friend which you would like to invite
+                </DialogContentText>
+                <InputWithLabel 
+                    value={mail}
+                    setValue={setMail}
+                    label='Mail'
+                    type='text'
+                    placeholder='Enter email address'
+                />
+            </DialogContent>
+            <DialogActions>
+                <PrimaryButton 
+                    onClick={handleSendInvitation}
+                    disabled={!isFormValid}
+                    label='Send'
+                    additionalStyle={{
+                        marginLeft: '15px',
+                        marginRight: '15px',
+                        marginBottom: '10px',
+                    }}
+                />
+            </DialogActions>
+        </Dialog>
+    );
+}
--- client/src/features/dashboard/DashboardPage.js
+++ client/src/features/dashboard/DashboardPage.js
@@ -1,4 +1,9 @@
 import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+import GroupsIcon from '@mui/icons-material/Groups';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
+import AddFriendDialog from './AddFriendDialog';
 
 const Wrapper = styled('div')({
     width: '100%',
@@ -9,13 +14,19 @@ const Wrapper = styled('div')({
 const SideBar = styled('div')({
     width: '72px',
     height: '100%',
-    backgroundColor: '#202225'
+    backgroundColor: '#202225',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
 });
 
 const FriendsSideBar = styled('div')({
     width: '224px',
     height: '100%',
-    backgroundColor: '#2F3136'
+    backgroundColor: '#2F3136',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
 });
 
 const AppBar = styled('div')({
@@ -35,11 +46,87 @@ const Messenger = styled('div')({
     flexGrow: 1,
 });
 
+const PendingInvitationList = styled('div')({
+    width: '100%',
+    height: '22%'
+})
+
+const FriendsList = styled('div')({
+    width: '100%',
+    flexGrow: 1
+});
+
 export default function DashboardPage() {
+
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const handleCloseAddFriendDialog = () => {
+        setIsDialogOpen(false);
+    };
+
+    const handleOpenAddFriendDialog = () => {
+        setIsDialogOpen(true);
+    }
+
     return (
         <Wrapper>
-            <SideBar></SideBar>
-            <FriendsSideBar></FriendsSideBar>
+            <SideBar>
+                <Button style={{
+                    width: '48px',
+                    height: '48px',
+                    borderRadius: '16px',
+                    margin: 0,
+                    padding: 0,
+                    marginTop: '10px',
+                    color: 'white',
+                    backgroundColor: '#5865F2'
+                }}>
+                    <GroupsIcon />
+                </Button>
+            </SideBar>
+            <FriendsSideBar>
+                <Button
+                    style={{
+                        marginTop: '10px',
+                        color: 'white',
+                        background: '#3ba55d',
+                        width: '80%',
+                        height: '30px',
+                    }}
+                    onClick={handleOpenAddFriendDialog}
+                >
+                    Add Friend
+                </Button>
+
+                <AddFriendDialog open={isDialogOpen} closeAddFriendDialog={handleCloseAddFriendDialog}/>
+
+                <Typography
+                    sx={{
+                        color: '#8e9297',
+                        fontSize: '14px',
+                        marginTop: '10px',
+                        textTransform: 'uppercase'
+                    }}
+                >
+                    Private Message
+                </Typography>
+
+                <FriendsList />
+
+                <Typography
+                    sx={{
+                        color: '#8e9297',
+                        fontSize: '14px',
+                        marginTop: '10px',
+                        textTransform: 'uppercase'
+                    }}
+                >
+                    Invitation
+                </Typography>
+
+                <PendingInvitationList />
+
+            </FriendsSideBar>
             <AppBar></AppBar>
             <Messenger></Messenger>
         </Wrapper>
